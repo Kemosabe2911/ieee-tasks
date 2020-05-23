abf03dad81f1cc964387e49c65c062c39fa193f7
Kemosabe2911 <stevinprince29@gmail.com>
2020-05-23 11:42:47 +0530

    Add index.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express= require('express');
 
 const app= express();
 
+app.use('/',require('./routes/index'));
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, console.log(`Server running at ${PORT}`));
