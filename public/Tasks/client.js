23ab56c379089b19d844987bc9382a9b78d1f154
Kemosabe2911 <stevinprince29@gmail.com>
2020-05-25 13:48:13 +0530

    Add fetchapi

--- public/Tasks/client.js
+++ public/Tasks/client.js
@@ -2,13 +2,16 @@ const name= document.getElementById('name');
 const email=  document.getElementById('email');
 const submit= document.getElementById('submit');
 
-function fetchData(){
+submit.addEventListener(submit,fetchData);
+
+function fetchData(e){
+    e.preventDefault();
     fetch("http://localhost:3000/Tasks/tasks1.html",{
         method: "POST"
     }
     )
-    .then(res=>res.json)
+    .then(res=>res.json())
     .then(data=>{
-        
+        console.log(data);
     })
 }
