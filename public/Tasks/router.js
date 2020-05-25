294b37d0e61014af70987070c7ba82e131492795
Kemosabe2911 <stevinprince29@gmail.com>
2020-05-25 13:57:46 +0530

    Update client.js

--- public/Tasks/router.js
+++
@@ -1,9 +0,0 @@
-const express= require('express');
-const router= express.Router();
-
-router.post('/Tasks/tasks1.html',(req,res)=>{
-    console.log(req.body);
-    res.send('Hello');
-});
-
-module.exports = router;
