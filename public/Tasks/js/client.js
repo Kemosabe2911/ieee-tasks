749d79a13bdb6f6b69194b4061766fc4a2d98b1e
Kemosabe2911 <stevinprince29@gmail.com>
2020-05-27 14:21:32 +0530

    Add axios

--- public/Tasks/js/client.js
+++ public/Tasks/js/client.js
@@ -9,15 +9,27 @@ myForm.addEventListener(submit,function(e){
 
 function fetchData(){
     
-    fetch("http://localhost:3000/Tasks/tasks1.html",{
+    /*fetch("http://localhost:3000/Tasks/tasks1.html",{
         method: "POST",
         headers:{
             'Content-Type': 'application/json'
         }
     }
     )
-    .then(res=>res.json())
+    .then((res)=> res.json())
     .then(data=>{
         console.log(data);
-    })
+    });*/
+    axios(
+        {
+            method: "POST",
+            url: "http://localhost:3000/Tasks/tasks1.html",
+            data: {
+                Name: name,
+                Email: email
+            }
+        }
+    )
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
 }
