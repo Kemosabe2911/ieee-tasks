b539fa16e61fab723446269e5712782d7d113e11
Kemosabe2911 <stevinprince29@gmail.com>
2020-05-30 19:44:43 +0530

    Add routes

--- routes/tasks.js
+++ routes/tasks.js
@@ -2,9 +2,14 @@ const express= require('express');
 const router= express.Router();
 
 //Task1
-router.get('/task1',(req,res) => res.send('task1'));
+router.get('/task1',(req,res) => res.render('./public/Tasks'));
 
 //Task2
 router.get('/task2',(req,res) => res.send('task2'));
+
+router.post('/task1.html', (req,res) =>{
+    const {name, emial}= req.body;
+    console.log(req.body);
+});
 
 module.exports = router;
